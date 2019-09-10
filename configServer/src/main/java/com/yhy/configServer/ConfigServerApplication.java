d795d6f56ac5b6f5ee6bb42e84b3d86bbd2a79fb
hongyuYaoChinese <18698655352@163.com>
2019-09-11 01:44:06 +0800

    精修

--- configServer/src/main/java/com/yhy/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/com/yhy/configServer/ConfigServerApplication.java
@@ -1,13 +1,11 @@
 package com.yhy.configServer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication	
-@EnableAutoConfiguration
 @EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigServerApplication {
