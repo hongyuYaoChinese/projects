99a4f5998d8cffc70793eeef0c59291481517595
hongyuYaoChinese <18698655352@163.com>
2019-09-12 02:59:54 +0800

    commit

--- getway/src/main/java/com/yhy/getway/GetwayApplication.java
+++ getway/src/main/java/com/yhy/getway/GetwayApplication.java
@@ -2,16 +2,16 @@ package com.yhy.getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableCircuitBreaker
+@EnableOAuth2Sso
 public class GetwayApplication {
 
 	public static void main(String[] args) {
--- oauthServer/src/main/java/com/yhy/oauthserver/OauthserverApplication.java
+++ oauthServer/src/main/java/com/yhy/oauthserver/OauthserverApplication.java
@@ -3,15 +3,44 @@ package com.yhy.oauthserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableResourceServer
 @EnableDiscoveryClient
-public class OauthserverApplication {
+public class OauthserverApplication extends WebSecurityConfigurerAdapter {
 
   public static void main(String[] args) {
     SpringApplication.run(OauthserverApplication.class, args);
+  }
+  
+  @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+      return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth
+      .inMemoryAuthentication()
+      .withUser("guest").password("guest").authorities("WRIGTH_READ")
+      .and()
+      .withUser("admin").password("admin").authorities("WRIGTH_READ", "WRIGTH_WRITE");
+  }
+  
+  @SuppressWarnings("deprecation")
+  @Bean
+  public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
   }
 
 }
