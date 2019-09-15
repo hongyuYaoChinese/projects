b8bfe2a9e5a30ab2ae47117dd63088c6858e6544
18698655352@163.com <ry19970418!>
2019-09-15 11:43:24 +0800

    修改oauthserver

--- getway/src/main/java/com/yhy/getway/WebSecurityConfigurer.java
+++ getway/src/main/java/com/yhy/getway/WebSecurityConfigurer.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableOAuth2Sso
+//@Configuration
+//@EnableOAuth2Sso
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
