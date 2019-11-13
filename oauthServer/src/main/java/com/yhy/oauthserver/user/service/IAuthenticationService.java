b56b2131b990f3a3207336b1c264b9aa84536ed3
yaohongyu <18698655352@163.com>
2019-11-13 20:10:58 +0800

    修改项目结构

--- oauthServer/src/main/java/com/yhy/oauthserver/user/service/IAuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package com.yhy.oauthserver.user.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface IAuthenticationService {
-	
-	Authentication getAuthentication();
-}
